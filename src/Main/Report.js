68936c686e31cf7e43d986655a1e41f5dd03c698
turib <oboroh12@gmail.com>
2022-07-26 12:53:26 +0900

    add

--- src/Main/Report.js
+++ src/Main/Report.js
@@ -160,9 +160,9 @@ const Submit = (props) => {
 
     await API.graphql(graphqlOperation(queryRds, {
       query: `insert into user_report (user_id, report_id, json_text) values (${user_id}, ${report_id}, JSON_OBJECT(
-        'text', '${json["text"]}', 'char_num', '${json["char_num"]}', 'word_num', '${json["word_num"]}', 'bind_rate', '${json["bind_rate"]}', 'char_rate', '${json["char_rate"]}', 'dupli_num', '${json["dupli_num"]}', 'end_unity', '${json["end_unity"]}','depend_mean', '${json["depend_mean"]}', 'proofreading', '${json["proofreading"]}', 'sentence_num', '${json["sentence_num"]}'}
+        'text', '${json["text"]}', 'char_num', '${json["char_num"]}', 'word_num', '${json["word_num"]}', 'bind_rate', '${json["bind_rate"]}', 'char_rate', '${json["char_rate"]}', 'dupli_num', '${json["dupli_num"]}', 'end_unity', '${json["end_unity"]}','depend_mean', '${json["depend_mean"]}', 'proofreading', '${json["proofreading"]}', 'sentence_num', '${json["sentence_num"]}'
       ));`
-    })).then((res) => res.text())
+    }))
     .catch((error) => {console.log('error')});
     
     navigation(`/result?user_id=${user_id}&report_id=${report_id}`);
